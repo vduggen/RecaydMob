8fcfd2719477e886a43ff8c0a4bfdefa67c77432
vduggen <zduggem@gmail.com>
2020-07-04 16:58:13 -0300

    Finalizado a parte do desktop

---
+++ script.js
@@ -0,0 +1,7 @@
+function openSidenav() {
+    document.getElementById('mobile-menu').style.width = '250px';
+}
+
+function closeSidenav() {
+    document.getElementById('mobile-menu').style.width = '0';
+}
